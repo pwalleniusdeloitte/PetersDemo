f45594dfc0b0c1db3b72f37b52b45aa27445a8e3
Peter Wallenius <pwallenius@outlook.dk>
2020-08-19 12:32:42 +0200

    Cal API with subscription key

--- ConsoleClient/ConsoleClient/Program.cs
+++ ConsoleClient/ConsoleClient/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
 
 namespace ConsoleClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string navn = "demo";
+            Console.WriteLine($"Sender: {navn}");
+            string svar = await MakeRequest(navn);
+            Console.WriteLine($"Svar: {svar}");
+            Console.ReadLine();
+        }
+
+        static async Task<string> MakeRequest(string navn)
+        {
+            string body = "{\"name\":\"" + navn + "\"}";
+            var client = new HttpClient();
+            var uri = "https://petersdemo.azure-api.net/PetersDemo/Capital";
+            HttpResponseMessage response;
+
+            // Request body
+            byte[] byteData = Encoding.UTF8.GetBytes(body);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                //Ved anvendelse af subscription key
+                content.Headers.Add("Ocp-Apim-Subscription-Key", "eeaf27ede20a4be7bb2104014d68e79d");
+                response = await client.PostAsync(uri, content);
+            }
+
+            return response.Content.ReadAsStringAsync().Result;
+
         }
     }
 }
